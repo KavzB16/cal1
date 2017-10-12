8a0d35497e6f1c8157d6a95363be7108b688cabf
Student <Student@ZAPRE-D00000155.DirectDelivery.com>
2017-10-12 10:58:48 +0200

    additional work with integer.parse and d1

--- src/cal1/Cal1.java
+++ src/cal1/Cal1.java
@@ -4,7 +4,9 @@ package cal1;
 public class Cal1 {
 
     public int Add(String s1, String s2){
-        return 0;
+        int d1 = Integer.parseInt(s1);
+        int d2 = Integer.parseInt(s2);
+        return d1+d2 ;
     }
    
     public static void main(String[] args) {
